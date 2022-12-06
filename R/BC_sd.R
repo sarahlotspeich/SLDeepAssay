03650c5168fcbf3d6a68fa88c8ac5a14a50394e1
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2022-12-06 08:21:27 -0500

    Write/document BC_sd()

---
+++ R/BC_sd.R
@@ -0,0 +1,31 @@
+#' Bias correction for IUPM estimator from single-dilution assay data
+#' @name BC_sd
+#' @param lambda Vector of DVL-specific parameters.
+#' @param M Total number of wells originally sequenced with the QVOA.
+#' @param q Proportion of p24-positive wells that underwent UDSA.
+#' @return A vector of the same length as \code{lambda}
+#'
+BC_sd = function(lambda, M, q) {
+  
+  # fisher information (n x n), inverse (n x n), and stacked inverse (n^2 x 1)
+  I = fisher_sd(lambda = lambda, M = M, q = q)
+  I.inv = solve(I)
+  vec.I.inv = as.vector(I.inv)
+  
+  # derivative of fisher information (n x n^2)
+  dI = deriv_fisher_sd(lambda = lambda, M = M, q = q)
+  
+  # expectation of third derivative of log-likelihood (n x n^2)
+  exp.d3 = exp_third_deriv_sd(lambda = lambda, M = M, q = q)
+  
+  # A matric (n x n^2)
+  A = - dI - 0.5 * exp.d3
+  
+  # bias correction
+  bc = I.inv %*% A %*% vec.I.inv
+  
+  ## Bias corrected MLE
+  lambda.bc = lambda - bc
+  
+  return(lambda.bc)
+}

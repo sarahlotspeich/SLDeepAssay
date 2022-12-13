bed7228b11c19ead4270a58db427e4bacb4df42d
Brian Richardson <brichson@ad.unc.edu>
2022-12-13 10:39:36 -0500

    Update documentation, change . to _, change dilution to u

--- R/fit_SLDeepAssay_md.R
+++ R/fit_SLDeepAssay_md.R
@@ -1,9 +1,9 @@
 #' Maximum likelihood estimator (MLE) for multiple-dilution assay data
 #' @name fit_SLDeepAssay_md
-#' @param assay List of data frames with assay data from each dilution level. Default is \code{NULL}.
-#' @param dilutions Vector of dilutions. Default is \code{NULL}.
-#' @param assay.summary (Optional) Instead of supplying \code{assay}, supply a summary of assay results. This summary should contain one row per dilution level and the following columns: M (total number of wells), n (number of distinct viral lineages \[DVL\]), MN (number of p24-negative wells), m (number of deep sequenced wells), Y1,..., Yn (counts of wells positive for DVL i, (i = 1,...,n), and dilutions (dilution levels, in millions of cells per well).
-#' @param bc Logical indicator for returning bias corrected MLE and CI. By default, the bias correction is not computed if n>40.
+#' @param assay_summary List of data frames with assay data from each dilution level. Default is \code{NULL}.
+#' @param u Vector of dilution levels, in millions of cells per well. Default is \code{NULL}.
+#' @param assay_summary (Optional) Instead of supplying \code{assay} and \code{u}, supply a summary of assay results in the form of a data frame. This summary should contain one row per dilution level and the following columns: M (total number of wells), n (number of distinct viral lineages \[DVL\]), MN (number of p24-negative wells), m (number of deep sequenced wells), Y1,..., Yn (counts of wells positive for DVL i, (i = 1,...,n), and u (dilution levels, in millions of cells per well).
+#' @param corrected Logical, if \code{corrected = TRUE} the bias-corrected MLE will be returned. If there are <= 40 DVL in \code{assay}, default is \code{corrected = TRUE}; otherwise default is \code{corrected = FALSE}.
 #' @param maxit The maximum number of iterations (passed to \code{optim}). Default is \code{maxit = 1E4}.
 #' @param lb Lower-bound on the IUPM (passed to \code{optim}). Default is \code{lb = 1E-6}.
 #' @param ub Upper-bound on the IUPM (passed to \code{optim}). Default is \code{ub = Inf}.
@@ -11,17 +11,17 @@
 #' \item{mle}{MLE}
 #' \item{se}{Standard error for the MLE}
 #' \item{ci}{95\% confidence interval for the MLE}
-#' \item{mle.bc}{Bias-corrected MLE}
-#' \item{ci.bc}{95\% confidence interval for the bias-corrected MLE}
+#' \item{mle_bc}{Bias-corrected MLE}
+#' \item{ci_bc}{95\% confidence interval for the bias-corrected MLE}
 #' @export
 #'
 #'
-fit_SLDeepAssay_md <- function(assay = NULL, dilutions = NULL, assay.summary,
-                      bc = NULL, maxit = 1E6, lb = 1E-6, ub = Inf) {
+fit_SLDeepAssay_md <- function(assay = NULL, u = NULL, assay_summary,
+                      corrected = NULL, maxit = 1E6, lb = 1E-6, ub = Inf) {
 
   # For each dilution level, compute summary data
   if (!is.null(assay)) {
-    assay.summary = vapply(X = 1:D, FUN.VALUE = numeric(7 + n),
+    assay_summary = vapply(X = 1:D, FUN.VALUE = numeric(7 + n),
                            FUN = function(d) {
                              M = ncol(assay[[d]])
                              n = nrow(assay[[d]])
@@ -30,37 +30,37 @@ fit_SLDeepAssay_md <- function(assay = NULL, dilutions = NULL, assay.summary,
                              m = MP - sum(is.na(colSums(assay[[d]])))
                              q = ifelse(MP == 0, 0, m / MP)
                              Y = rowSums(assay[[d]], na.rm = TRUE)
-                             return((c("dilution" = dilutions[d], "M" = M, "n" = n,
+                             return((c("u" = u[d], "M" = M, "n" = n,
                                        "MN" = MN, "MP" = MP, "m" = m, "q" = q, "Y" = Y)))
                            })
-    assay.summary = as.data.frame(t(assay.summary))
+    assay_summary = as.data.frame(t(assay_summary))
   }
 
   # Indicator for whether bias correction should be computed:
   # user specified value if provided, else yes if n <= 40
-  bc = ifelse(test = is.null(bc),
-              yes = assay.summary$n[1] <= 40,
-              no = bc)
+  corrected = ifelse(test = is.null(correctedc),
+              yes = assay_summary$n[1] <= 40,
+              no = corrected)
 
   # Fit MLE
-  optimization = optim(par = rep(0, assay.summary$n[1]),
+  optimization = optim(par = rep(0, assay_summary$n[1]),
                        fn = function(t)
-                         loglik_md(tau = t, assay.summary = assay.summary),
+                         loglik_md(tau = t, assay_summary = assay_summary),
                        gr = function(t)
-                         gloglik_md(tau = t, assay.summary = assay.summary),
+                         gloglik_md(tau = t, assay_summary = assay_summary),
                        method = "L-BFGS-B",
                        control = list(maxit = maxit),
-                       lower = rep(lb, assay.summary$n[1]),
-                       upper = rep(ub, assay.summary$n[1]),
+                       lower = rep(lb, assay_summary$n[1]),
+                       upper = rep(ub, assay_summary$n[1]),
                        hessian = T)
 
   ### parameter estimate
-  tau.hat = optimization$par
-  Tau.hat = sum(tau.hat) # MLE of the IUPM
+  tau_hat = optimization$par
+  Tau_hat = sum(tau_hat) # MLE of the IUPM
 
   # Fisher information matrix
-  I <- fisher_md(tau = tau.hat, M = assay.summary$M, q = assay.summary$q,
-                 dilutions = assay.summary$dilution)
+  I <- fisher_md(tau = tau_hat, M = assay_summary$M, q = assay_summary$q,
+                 u = assay_summary$u)
 
   # inverse of fisher information
   cov <- solve(I)
@@ -69,32 +69,31 @@ fit_SLDeepAssay_md <- function(assay = NULL, dilutions = NULL, assay.summary,
   se <- sqrt(sum(cov))
 
   ### confidence interval
-  ci = exp(c(log(Tau.hat) + c(-1, 1) * (qnorm(0.975) * se / Tau.hat)))
+  ci = exp(c(log(Tau_hat) + c(-1, 1) * (qnorm(0.975) * se / Tau_hat)))
 
   # For large n, do not compute bias correction unless user overrides
   if (bc == F) {
 
-    Tau.hat.bc = NA
-    se.bc = NA
-    ci.bc = NA
+    Tau_hat_bc = NA
+    ci_bc = NA
 
   } else {
 
     ### bias correction
-    tau.hat.bc <- BC_md(tau = tau.hat, M = assay.summary$M, q = assay.summary$q,
-                        dilutions = assay.summary$dilution)
+    tau_hat_bc <- BC_md(tau = tau_hat, M = assay_summary$M, q = assay_summary$q,
+                        u = assay_summary$u)
 
     # bias-corrected MLE for Tau
-    Tau.hat.bc <- sum(tau.hat.bc)
+    Tau_hat_bc <- sum(tau_hat_bc)
 
     # bias corrected CI
-    ci.bc <- exp(c(log(Tau.hat.bc) + c(-1, 1) *
-                     (qnorm(0.975) * se / Tau.hat.bc)))
+    ci_bc <- exp(c(log(Tau_hat_bc) + c(-1, 1) *
+                     (qnorm(0.975) * se / Tau_hat_bc)))
   }
 
-  return(list("mle" = Tau.hat,
+  return(list("mle" = Tau_hat,
               "se" = se,
               "ci" = ci,
-              "mle.bc" = Tau.hat.bc,
-              "ci.bc" = ci.bc))
+              "mle_bc" = Tau_hat_bc,
+              "ci_bc" = ci_bc))
 }

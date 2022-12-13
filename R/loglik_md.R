41374b6cce1a53d96ccbd810b515a2fd196163ac
Brian Richardson <brichson@ad.unc.edu>
2022-12-13 10:24:09 -0500

    Update documentation and change . to _

--- R/loglik_md.R
+++ R/loglik_md.R
@@ -1,23 +1,23 @@
 #' Log-likelihood for IUPM from multiple-dilution assay data
 #' @name loglik_md
-#' @param tau Parameter
-#' @param assay.summary Data
-#' @return A scalar
+#' @param tau Vector of DVL-specific IUPM parameters
+#' @param assay_summary Summary of assay data from multiple dilutions
+#' @return A scalar value of the log-likelihood
 #'
-loglik_md = function(tau, assay.summary) {
+loglik_md = function(tau, assay_summary) {
   # Total number of dilutions
-  D = nrow(assay.summary)
+  D = nrow(assay_summary)
 
   # Compute negative log-likelihoods for each dilution
   logliks_by_dilution <- vapply(X = 1:D,
                                 FUN.VALUE = numeric(1),
                                 FUN = function(d) {
-                                  as.numeric(loglik_sd(l = assay.summary$dilution[d] * tau,
-                                                       n = assay.summary$n[d],
-                                                       M = assay.summary$M[d],
-                                                       MP = assay.summary$MP[d],
-                                                       m = assay.summary$m[d],
-                                                       Y = assay.summary[d, grepl("Y", colnames(assay.summary))]))
+                                  as.numeric(loglik_sd(l = assay_summary$dilution[d] * tau,
+                                                       n = assay_summary$n[d],
+                                                       M = assay_summary$M[d],
+                                                       MP = assay_summary$MP[d],
+                                                       m = assay_summary$m[d],
+                                                       Y = assay_summary[d, grepl("Y", colnames(assay_summary))]))
                                 })
 
   # Return the sum of (negative) log-likelihoods

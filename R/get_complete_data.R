5ba6fd4ea889132dfa7a99b028db1e63ff8ec274
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-06-13 09:22:07 -0400

    Create get_complete_data_big()

--- R/get_complete_data.R
+++ R/get_complete_data.R
@@ -58,3 +58,73 @@ get_complete_data = function(Wstar, Zstar) {
   
   return(cd_long)
 }
+
+get_complete_data_big = function(Wstar, Zstar) {
+  # Save useful constants 
+  M = length(Wstar) ## number of replicate wells 
+  MP = sum(Wstar) ## number p24-positive replicate wells 
+  n = nrow(Zstar) ## number of DVLs detected
+  m = sum(!is.na(colSums(Zstar))) ## number of p24-positive, sequenced replicate wells 
+  
+  # Transform Z* in preparation to augment with cd
+  Zstar_seq = t(Zstar) ## transpose to get rows per well, cols per DVL
+  ## then keep only sequenced wells (rows) 
+  if (n == 1) { ### if only one DVL detected, need to force Zstar_seq to a column
+    Zstar_seq = matrix(data = Zstar_seq[complete.cases(Zstar_seq), drop = FALSE], 
+                       ncol = 1) 
+  } else if (m == 1) { ### if only one well sequenced, need to force Zstar_seq to a row
+    Zstar_seq = matrix(data = Zstar_seq[complete.cases(Zstar_seq), drop = FALSE], 
+                       nrow = 1) 
+  } else {
+    Zstar_seq = Zstar_seq[!is.na(colSums(Zstar)), ] ## keep only sequenced wells (rows) 
+  }
+  colnames(Zstar_seq) = paste0("zstar", 1:n) ## name cols {z*1, ..., z*n}
+  
+  # Create matrix with "complete data" for sequenced wells
+  ## Includes all possible combinations of true Z
+  cd_seq = expand.grid(replicate(n = n, expr = c(0, 1), simplify = F))
+  colnames(cd_seq) = paste0("z", 1:n)  ## name cols {z1, ..., zn}
+  ## Compute true W from true Z
+  cd_seq$w = as.numeric(rowSums(cd_seq) > 0)
+
+  # Augment complete data with observed data 
+  ## For sequenced wells 
+  cd_seq_long = cbind(j = rep(x = 1:m, each = nrow(cd_seq)),
+                      cd_seq[rep(x = 1:nrow(cd_seq), times = m), ], 
+                      Zstar_seq[rep(x = 1:m, each = nrow(cd_seq)), , drop = F], 
+                      wstar = rep(x = Wstar[1:m], each = nrow(cd_seq)))
+  
+  if ((M - m) > 0) {
+    ## For unsequenced wells 
+    cd_unseq_long = data.frame(cbind(j = rep(x = (m + 1):M, each = 2), 
+                               w = rep(x = c(0, 1), times = (M - m)),
+                               wstar = rep(x = Wstar[(m + 1):M], each = 2)))
+  } else {
+    cd_unseq_long = data.frame(j = NA, w = NA, wstar = NA)
+  }
+  
+  return(list(complete_seq = cd_seq_long, 
+              complete_unseq = cd_unseq_long))
+}
+
+# get_complete_data_big = function(n, m, Wstar_seq, Zstar_seq) {
+#   # Create 2 ^ n x n matrices of DVL combinations
+#   z = data.matrix(expand.grid(
+#     replicate(n = n, 
+#               expr = c(0, 1), 
+#               simplify = F)
+#     )
+#   )
+#   
+#   # Reorder to put the row of all 0s first
+#   order_z = order(rowSums(z) != 0)
+#   z = z[order_z, ]
+#   
+#   # Create matrix of complete data for sequenced wells
+#   # Augment complete data with observed data 
+#   ## For sequenced wells 
+#   cd_seq_long = cbind(j = rep(x = 1:m, each = nrow(z)),
+#                       z[rep(x = 1:nrow(z), times = m), ], 
+#                       Zstar_seq[rep(x = 1:m, each = nrow(z)), , drop = F], 
+#                       wstar = rep(x = Wstar_seq, each = nrow(z)))
+# }

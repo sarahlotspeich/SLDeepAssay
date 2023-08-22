a385ffc5e5fde3810117fe32847d2f48dad58fa4
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-08-22 13:35:18 -0400

    Rename for Table S5

--- tables_revision/TableS5_revision.R
+++ tables_revision/TableS5_revision.R
@@ -17,10 +17,10 @@ library(kableExtra)
 library(magick)
 
 # Load data
-tableS8_data = read.csv("https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/real_data_application_revision/tableS8_data.csv")
+tableS8_data = read.csv("https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/real_data_application_revision/tableS5_data.csv")
 
 ## create Table S8
-tableS8_data %>% 
+tableS8_data |> 
   kable(format = "latex",
         booktabs = T,
         escape = F,
@@ -30,11 +30,11 @@ tableS8_data %>%
                                 "Without UDSA\n(Multiple Dilutions)",
                                 "With UDSA\n(Single Dilution)",
                                 "With UDSA\n(Multiple Dilutions)"),
-                              align = "c")) %>%
-  kable_classic_2(full_width = F) %>%
-  kable_styling(font_size = 10) %>% 
-  add_header_above(c(" " = 2, "MLE (95% Confidence Interval)" = 3), bold = T) %>% 
-  row_spec(row = 0, bold = TRUE) %>% 
+                              align = "c")) |>
+  kable_classic_2(full_width = F) |>
+  kable_styling(font_size = 10) |> 
+  add_header_above(c(" " = 2, "MLE (95% Confidence Interval)" = 3), bold = T) |> 
+  row_spec(row = 0, bold = TRUE) |> 
   collapse_rows(columns = 1, latex_hline = "major", valign = "middle") -> tableS8
 
 tableS8

bb0218f967eda3321ca8404134b4f4f6de178885
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-08-22 13:25:07 -0400

    rename table scripts

--- tables_revision/TableS1_revision.R
+++ tables_revision/TableS1_revision.R
@@ -1,53 +1,70 @@
-## Title: Table S1: IUPM overdispersion likelihood ratio test sim results
+## Title: Table S1: Single dilution setting simulations (non-constant Tau)
 
-## Author:  Brian Richardson
+## Date: 2023/07/18
 
-## Date:  2023/06/20
+## Author: Sarah Lotspeich
 
-## Purpose: Produce Table S1 in SLDeepAssay paper
+## Purpose: produce a table to summarize simulation data from the single dilution setting
 
-## Prepare workspace
-rm(list=ls())
-setwd(here::here())
+## Input: https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/sim_data/sd_sim_data.csv (produced by sims/SIMS-SINGLE-DILUTIONS.R)
 
-## load packages
+## Output: Latex table
+
+# load packages
+library(kableExtra)
 library(dplyr)
 library(tidyr)
-library(kableExtra)
-library(magick)
-library(emdbook)
 
-## Load data
-overdispersion_sim_data = read.csv("https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/sim_data/overdispersion_sim_data.csv") %>% 
-  mutate(Gamma = 1/k,
-         gamma = ordered(Gamma))
+# load data
+setwd(here::here())
+sd_sim_data = read.csv("https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/sim_data/sd_sim_data.csv")
 
-## check for re-run sims
-overdispersion_sim_data %>% 
-  group_by(M.label, gamma) %>% 
-  summarize(mean.error = mean(error),
-            mean.redo.all = mean(num_redo_all, na.rm = T),
-            mean.redo.none = mean(num_redo_none, na.rm = T)) %>% 
-  kable(col.names = c("M", "gamma", "Prop. Error",
-                      "Prop. Redo All", "Prop. Redo None")) %>% 
-  kable_classic(full_width = F)
+# Function format_nums() rounds and formats numbers for LaTex table
+format_nums = function(x, digits = 2) {
+  paste0("$", format(round(x, digits = digits), nsmall = digits) , "$")
+}
 
-# 95% quantile of null distribution of LRT stat
-cutoff <- emdbook::qchibarsq(q = 0.95, df = 1, mix = 0.5)
+# check number of sims removed
+sd_sim_data |>
+  select(Message) |>
+  sum()
 
-## create Table S1
-overdispersion_sim_data %>% 
-  group_by(M, M.label, gamma) %>% 
-  summarise(power = mean(lrt_stat > cutoff, na.rm = T)) %>% 
-  ungroup() %>% 
-  select(M.label, gamma, power) %>% 
-  kable(col.names = c("M", "gamma", "Power of LRT"),
-        format = "latex",
-        digits = 3,
-        align = rep("c", 3),
-        booktabs = TRUE,
-        linesep = c("", "", "", "\\addlinespace"),
-        escape = FALSE) %>% 
-  row_spec(row = 0, bold = TRUE) -> tableS1
+# check for replicates MLE and BC-MLE without UDSA were infinite (these are excluded)
+## these counts are noted in the Table footnote
+sd_sim_data |> 
+  filter(abs(Est) == Inf) |> 
+  group_by(Method, Tau, constant_Tau) |> 
+  summarize(reps = n())
 
-tableS1
+# summarize sim results
+sd_sim_summ = sd_sim_data |>
+  mutate(
+    Message = ifelse(abs(Est) == Inf, 1, Message), 
+    Est = ifelse(abs(Est) == Inf, NA, Est)) |> 
+  group_by(constant_Tau, Tau, n, M, q, Method) |>
+  summarise(n_removed = sum(Message),
+            rel_bias = mean(Est - Tau, na.rm = TRUE),
+            ase = mean(SE, na.rm = TRUE),
+            ese = sd(Est, na.rm = TRUE),
+            cp = mean(LB <= Tau & Tau <= UB, na.rm = TRUE)) |>
+  pivot_wider(id_cols = c("M", "n", "q", "Tau", "constant_Tau"),
+              names_from = "Method",
+              values_from = c("n_removed", "rel_bias", "ase", "ese", "cp"))
+
+analysis_cols = as.vector(outer(c("rel_bias", "ase", "ese", "cp"),
+                                as.vector(outer(c("_MLE", "_BCMLE"), c("_woUDSA", "_wUDSA"), paste0)), paste0))
+
+col_order = c("n", "M", "q", analysis_cols)
+
+# produce table with simulation summary
+sd_sim_summ |> 
+  filter(constant_Tau == 0, Tau == 1) |> # Subset to columns with constant Tau
+  dplyr::ungroup() |>
+  dplyr::select(all_of(col_order)) |>
+  dplyr::mutate_at(analysis_cols, format_nums) |>
+  magrittr::set_colnames(c("$\\pmb{n'}$", "M", "q", rep(c("Bias", "ASE", "ESE", "CP"), times = 4))) |>
+  kable(format = "latex", digits = 3, align = c(rep("c", 3), rep("r", 16)),
+        booktabs = TRUE, linesep = c("", "", "\\addlinespace"), escape = FALSE) |>
+  add_header_above(header = c(" " = 3, "MLE" = 4, "Bias-Corrected MLE" = 4, "MLE" = 4, "Bias-Corrected MLE" = 4), bold = TRUE) |>
+  add_header_above(header = c(" " = 3, "Without UDSA" = 8, "With UDSA" = 8), bold = TRUE) |>
+  row_spec(row = 0, bold = TRUE)

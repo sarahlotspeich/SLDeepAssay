15b1df6c1b1f4e248ed6549a38a50365068df972
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-08-28 12:22:23 -0400

    Rewrite SIM-IMPERFECT-C1.R to use Ys from the real data

--- real_data_application_revision/SIM-IMPERFECT-C1.R
+++ real_data_application_revision/SIM-IMPERFECT-C1.R
@@ -1,11 +1,14 @@
 library(SLDeepAssay)
 
 # Define parameters based on Subject C1
-M = c(36, 6, 6) # Total number of wells
-MP = c(4, 0, 0) # Number of QVOA-positive wells
-n = 12 # Number of detected DVLs
-u = c(2.5, 0.5, 0.1) # Dilutions in millions of cells per well
-q = c(1, 0, 0) # Proportion of p24-positive wells to undergo UDSA
+subject_params = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/SLDeepAssay/main/real_data_application_revision/data/UDSA_MD_C1.csv")
+
+M = subject_params$M # Total number of wells
+MP = subject_params$MP # Number of QVOA-positive wells
+n = subject_params$n[subject_params$deepseq == 1] # Number of detected DVLs
+u = subject_params$u # Dilutions in millions of cells per well
+m = subject_params$m # Number of p24-positive wells to undergo UDSA
+Y = subject_params[subject_params$deepseq == 1, -c(1:9)]
 
 ## Reproducibility
 set.seed(123)
@@ -23,11 +26,9 @@ for (d in 1:length(u)) {
              ncol = M[d])
   
   ## If deep sequencing was done, replace NA values for m deep-sequenced wells
-  if (q[d] > 0) {
-    ## Simulate the number of wells infected with each DVL 
-    Z_rowsum = sample(x = 1:MP[d], 
-                      size = n, 
-                      replace = TRUE)
+  if (m[d] > 0) {
+    ## Take the number of wells infected with each DVL from the real data
+    Z_rowsum = Y
     
     ## For each DVL... 
     for(i in 1:nrow(Z)) {
@@ -53,7 +54,7 @@ fit1 = fit_SLDeepAssay_md_imperfect(assay_md = temp,
                                     spec_QVOA = 1, 
                                     sens_UDSA = 1, 
                                     spec_UDSA = 1)
-with(fit1, c(mle, convergence, message))
+#with(fit1, c(mle, convergence, message))
 
 # Original likelihood (naive IUPM estimator)
 assay_summary = vapply(X = 1:length(u),
@@ -72,4 +73,28 @@ assay_summary = vapply(X = 1:length(u),
 assay_summary = as.data.frame(t(assay_summary))
 fit2 = fit_SLDeepAssay_md(assay_summary = assay_summary, 
                           corrected = FALSE)
-fit2$mle
+
+# Try a variety of sensitivity/specificity combinations
+try_sens_spec = expand.grid(sens_qvoa = seq(0.8, 1, by = 0.1), 
+                            spec_qvoa = seq(0.8, 1, by = 0.1), 
+                            sens_udsa = seq(0.8, 1, by = 0.1), 
+                            spec_udsa = seq(0.8, 1, by = 0.1),
+                            mle = NA, 
+                            ci_lb = NA, 
+                            ci_ub = NA
+                            )
+
+for (t in 1:nrow(try_sens_spec)) {
+  # Estimate IUPM 
+  ## New likelihood (corrected IUPM estimator for imperfect assays)
+  fit1 = fit_SLDeepAssay_md_imperfect(assay_md = temp,
+                                      u = u, 
+                                      sens_QVOA = try_sens_spec$sens_qvoa[t], 
+                                      spec_QVOA = try_sens_spec$spec_qvoa[t], 
+                                      sens_UDSA = try_sens_spec$sens_udsa[t], 
+                                      spec_UDSA = try_sens_spec$spec_udsa[t])
+  
+  try_sens_spec$mle[t] = fit1$mle
+  try_sens_spec$ci_lb[t] = fit1$ci[1]
+  try_sens_spec$ci_ub[t] = fit1$ci[2]
+}

82d6024608c335083c7fe7402e3bbd6f520b1098
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-08-30 13:06:49 -0400

    Correct FUN.VALUE = numeric(n)  in call to vapply()

--- R/gloglik_md_imperfect.R
+++ R/gloglik_md_imperfect.R
@@ -4,14 +4,14 @@ gloglik_md_imperfect = function(tau, u, complete_data_md) {
   
   # Compute gradients of log-likelihoods for each dilution
   glogliks_by_dilution = vapply(X = 1:D,
-                               FUN.VALUE = numeric(1),
-                               FUN = function(d) {
-                                 as.numeric(
-                                   gloglik_sd_imperfect(
-                                     l = u[d] * tau,
-                                     complete_data = complete_data_md[[d]]
-                                   )
-                                 )
+                                FUN.VALUE = numeric(n),
+                                FUN = function(d) {
+                                  as.numeric(
+                                    gloglik_sd_imperfect(
+                                      l = u[d] * tau,
+                                      complete_data = complete_data_md[[d]]
+                                    )
+                                  )
                                }
   )
   

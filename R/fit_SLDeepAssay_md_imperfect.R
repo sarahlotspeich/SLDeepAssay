983829a016c7975443f212f4d47740691bfc106b
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-09-01 13:43:01 -0400

    Put the gradient back into fit_SLDeepAssay_md_imperfect() and change starting values to 1E-6 (instead of 0)

--- R/fit_SLDeepAssay_md_imperfect.R
+++ R/fit_SLDeepAssay_md_imperfect.R
@@ -70,9 +70,9 @@ fit_SLDeepAssay_md_imperfect = function(assay_md, u, sens_QVOA = 1, spec_QVOA = 
   ########################################################################################
   # Find MLEs ############################################################################
   ########################################################################################
-  optimization = optim(par = rep(0, n[1]),
+  optimization = optim(par = rep(1E-6, n[1]),
                        fn = loglik_md_imperfect, 
-                       gr = gloglik_md_imperfect, 
+                       #gr = gloglik_md_imperfect, 
                        u = u, 
                        complete_data_md = cd_md,
                        method = "L-BFGS-B",

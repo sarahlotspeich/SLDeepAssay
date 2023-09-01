65a06f247515c0ee9e41961ef8ce791bff1c05f4
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-09-01 08:58:59 -0400

    Move all functions/helpers for imperfect assays log-likelihood into loglik_imperfect.R

--- R/loglik_imperfect.R
+++ R/loglik_imperfect.R
@@ -119,3 +119,65 @@ get_pZ = function(complete_data, l, exclude_DVL = NA) {
   }
   return(pZ)
 }
+
+loglik_sd_imperfect = function(l, complete_data) {
+  # Log-likelihood contributions
+  ## Sequenced wells 
+  if (!any(is.na(complete_data$complete_seq$j)) & nrow(complete_data$complete_seq) > 0) {
+    ### Calculate P(Z) for all rows in sequenced wells' complete data 
+    complete_data$complete_seq$pZ = get_pZ(complete_data = complete_data$complete_seq, 
+                                           l = l) 
+    ### Sum P(W*|W)P(Z*|Z)P(Z) over Z to get P(W*, Z*)
+    complete_data$complete_seq$sum_to_pObs = with(complete_data$complete_seq, 
+                                                  pWstarGivW * pZstarGivZ * pZ)
+    pObs = rowsum(x = complete_data$complete_seq[, "sum_to_pObs"], 
+                  group = complete_data$complete_seq[, "j"])
+    ### Replace pObs = 0 with 1 to avoid infinite log-likelihood 
+    pObs[pObs == 0] = 1
+    ### Re-calculate log-likelihood 
+    ll = sum(log(pObs))
+  } else {
+    ll = 0
+  }
+  
+  ## Unsequenced wells
+  if (!any(is.na(complete_data$complete_unseq$j)) & nrow(complete_data$complete_unseq) > 0) {
+    ### Calculate P(W) for all rows in unsequenced wells' complete data 
+    complete_data$complete_unseq$pW = get_pW(complete_data = complete_data$complete_unseq, 
+                                             l = l) 
+    ### Sum P(W*|W)P(W) over W to get P(W*)
+    complete_data$complete_unseq$sum_to_pObs = with(complete_data$complete_unseq, 
+                                                    pWstarGivW * pW)
+    pObs = rowsum(x = complete_data$complete_unseq[, "sum_to_pObs"], 
+                  group = complete_data$complete_unseq[, "j"])
+    # Replace pObs = 0 with 1 to avoid infinite log-likelihood 
+    pObs[pObs == 0] = 1
+    # Re-calculate log-likelihood 
+    ll = ll + sum(log(pObs))
+  } 
+  
+  return(-ll)
+}
+
+loglik_md_imperfect = function(tau, u, complete_data_md) {
+  # Total number of dilutions
+  D = length(complete_data_md)
+  
+  # Compute negative log-likelihoods for each dilution
+  logliks_by_dilution = vapply(X = 1:D,
+                               FUN.VALUE = numeric(1),
+                               FUN = function(d) {
+                                 as.numeric(
+                                   loglik_sd_imperfect(
+                                     l = u[d] * tau,
+                                     complete_data = complete_data_md[[d]]
+                                   )
+                                 )
+                               }
+  )
+  
+  # Return the sum of (negative) log-likelihoods
+  ll = sum(logliks_by_dilution)
+  
+  return(ll)
+}
